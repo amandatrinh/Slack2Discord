0327db138754c19fabdfc0999534ca72f52abba2
Joshua Kuan <jckuan1@uci.edu>
2020-04-01 02:02:08 -0700

    made secret get from env vars

--- secret.py
+++ secret.py
@@ -1,4 +1,6 @@
-SLACK_SIGNING_SECRET = ''
-CLIENT_ID = ''
-CLIENT_SECRET = ''
-MONGO_URI = ''
+import os
+SLACK_SIGNING_SECRET = os.environ.get('SLACK_SIGNING_SECRET', '')
+CLIENT_ID = os.environ.get('CLIENT_ID', '')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET', '')
+MONGO_URI = os.environ.get('MONGO_URI', '')
+
